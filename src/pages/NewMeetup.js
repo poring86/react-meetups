fa2ba1c8276b57eb26e7e5db685f22796c49561b
Matheus Lino <matheuslino86a@gmail.com>
2021-03-22 21:27:43 -0400

    using firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,28 @@
+import { useHistory } from 'react-router-dom'
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+    const history = useHistory()
+    function onAddMeetupHandler(meetupData) {
+        fetch(
+            "https://react-meetup-bee0f-default-rtdb.firebaseio.com/meetups.json",
+            {
+                method:'POST',
+                body: JSON.stringify(meetupData),
+                headers:{
+                    'Content-Type' : 'application/json'
+                }
+            }
+        ).then(() => {
+            history.push('/')
+        });
+    }
+
     return (
         <section>
             <h1>Add New Meetup</h1>
-            <NewMeetupForm />
+            <NewMeetupForm onAddMeetup={onAddMeetupHandler} />
         </section>
     );
 }
