30795aa52e7e763d1f15e9e304f7593e254c873f
Matheus Lino <matheuslino86a@gmail.com>
2021-03-23 18:54:02 -0400

    context provider added

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,8 +1,29 @@
+import { useContext } from "react";
 import Card from "../ui/Card";
 import classes from "./MeetupItem.module.css";
 
-function MeetupItem(props){
-    return(
+import FavoritesContex from '../../store/favorites-context'
+
+function MeetupItem(props) {
+    const favoritesCtx = useContext(FavoritesContex)
+
+    const itemIsFavorite = favoritesCtx.itemIsFavorite(props.id)
+
+    function toggleFavoriteStatusHandler() {
+        if(itemIsFavorite){
+            favoritesCtx.removeFavorite(props.id)
+        }
+        else{
+            favoritesCtx.addFavorite({
+                id: props.id,
+                title: props.title,
+                description: props.description,
+                image: props.image,
+                address: props.address
+            })
+        }
+    }
+    return (
         <li className={classes.item}>
             <Card>
                 <div className={classes.image}>
@@ -14,13 +35,13 @@ function MeetupItem(props){
                     <p>{props.description}</p>
                 </div>
                 <div className={classes.actions}>
-                    <button>
-                        To Favorites
+                    <button onClick={toggleFavoriteStatusHandler}>
+                        {itemIsFavorite ? 'Remove from Favorites' : 'To Favorites'}
                     </button>
                 </div>
             </Card>
         </li>
-    )
+    );
 }
 
-export default MeetupItem
+export default MeetupItem;
